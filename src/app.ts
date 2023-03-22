5d7e135cb9fc5b01bdef98784e687c6cc833e50c
Sylwia <stefansylwia@interia.pl>
2023-03-22 15:18:43 +0100

    change the URL origin

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ export const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors({
-    origin: ['http://localhost:3000', "https://mern-task-app.onrender.com"],
+    origin: ['http://localhost:3000', "https://mern-task-app-68dr.onrender.com/"],
     methods: ['GET', 'POST', 'PUT', 'DELETE', ],
 }))
 
